5b5617fcef59c94e88dcf4958e18af1f3b0d8435
Gabriele-66 <gabrielerecchia70@gmail.com>
2022-02-21 18:09:43 +0100

    finish step 5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../model/hero';
-import { HEROES } from 'src/assets/mock-heroes';
 import { HeroService } from '../service/hero.service';
 import { MessageService } from '../service/message.service';
 
@@ -12,17 +12,11 @@ import { MessageService } from '../service/message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
       this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroComponents: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from '../model/hero';
 import { HEROES } from 'src/assets/mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -11,10 +12,17 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroSerice: fetched heroes');
     return heroes;
   }
 
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
